638bd66a4573c0d687517a4203a3f17636722ce4
littleorange1017 <1941396027@qq.com>
2026-06-04 19:52:15 +0800

    feat(common): 添加会员视图对象
    - 创建 MemberVo 类作为会员数据传输对象
    - 添加 username 字段用于存储用户名
    - 添加 avatar_url 字段用于存储头像链接
    - 实现 Serializable 接口支持序列化
    - 使用 Lombok @Data 注解简化 getter/setter 方法

---
+++ gulimall-Parent/common/src/main/java/vo/MemberVo.java
@@ -0,0 +1,11 @@
+package vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class MemberVo implements Serializable {
+    private String username;
+    private String avatar_url;
+}
